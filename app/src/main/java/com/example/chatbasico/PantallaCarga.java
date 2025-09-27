95c302795a06a12433e316e83de7cff1e164780d
Erick De la Rosa <eddv0001@ce.pucmm.edu.do>
2025-09-27 02:40:05 -0400

    creacion de visuales de pantalla de carga y de inicio de registro

---
+++ app/src/main/java/com/example/chatbasico/PantallaCarga.java
@@ -0,0 +1,28 @@
+package com.example.chatbasico;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PantallaCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(PantallaCarga.this,registrarte.class));
+                finish();
+            }
+        },4000);
+    }
+}
